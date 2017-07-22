298fcaffc25e672cda65b4071df3bcb0a21e876c
Rohit Yadav <rohit.yadav@manheim.com>
2017-07-21 22:48:13 -0400

    Intial Commit foodie-config-service

---
+++ src/main/java/com/config/server/foodieconfigservice/FoodieConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.config.server.foodieconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class FoodieConfigServiceApplication {
+	public static void main(String[] args) {
+        SpringApplication.run(FoodieConfigServiceApplication.class, args);
+	}
+}
